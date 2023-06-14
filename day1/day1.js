42f3f4920707a51551d4187568e72487164a692d
tran van duc <22010159@st.phenikaa-uni.edu.vn>
2023-06-14 21:08:32 +0700

    video1-14

--- day1/day1.js
+++ day1/day1.js
@@ -1,2 +1,36 @@
 console.log('xin chao viet nam')
 console.log('tran van duc')
+var fullName = 'tran van duc';
+console.log(fullName);
+let a = 3;
+let b = 4;
+let c = a ** b;//a mu b
+console.log('gia tri cua c la' ,c);
+
+// setInterval(function() {
+//    alert('Day la tieu de');
+//    confirm('xac nhan do tuoi')
+//    prompt('Nhap vao thong tin')
+// },5000);
+//10
+var number = 5;
+var giatri = number++;
+console.log('output:',giatri);
+console.log('output:', number);
+var d = 7;
+var output = d++ + --d;
+//7 + 8-1 = 14;
+console.log('gia tri cua output:',output);
+console.log('Gia tri cua d :', d);
+
+//12
+var firstname = 'duc';
+var lastname = 'tran';
+console.log(firstname + ' '+lastname);
+var hoten = 'duc';
+hoten = hoten +' tran';
+console.log(hoten);
+var j = 0;
+var i = 1;
+var sosanh = j<i;
+console.log(sosanh);
